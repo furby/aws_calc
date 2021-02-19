680caefd52dfd99ef3912e5f700794419ac42a77
Guy Tallon <guy.tallon@benevolent.ai>
2021-02-19 12:52:54 +0000

    variables renames and extraneous comments removed

--- aws_calc.py
+++ aws_calc.py
@@ -1,17 +1,17 @@
 import json
 
 #frobnicate_controller_instance_type
-fc='r5.2xlarge'
-#frobnicate_multi_controller
-fmc=2
+fctrl='r5.2xlarge'
+#frobnicate_multi_controller (true = 2)
+fmctrl=2
 #frobnicate_worker_count
-fwc=5
+fworkerct=5
 #frobnicate_worker_instance_type
-fw='m5.xlarge'
+fworker='m5.xlarge'
 #smash_controller_instance_type
-sc='m5.xlarge'
+sctrl='m5.xlarge'
 #smash_worker_count
-swc=30
+sworkerct=30
 #smash_worker_instance_type
 sworker='i3.2xlarge'
 
@@ -19,30 +19,30 @@ os = "Windows"
 term = 'OnDemand'
 region = "EU (London)"
 
+#possible input
 instance = input("The type of instance. e.g. t2.xlarge: ")
 keyvalue = 'instanceType'
 
 
-print("Prices of AWS")
+print("AWS Price calculator\n")
 
-#open JSON file and assign variable
+
 with open('snippet.json') as data:
   sampleDict = json.load(data)
 
+# check if keyvalue exists and mathces what you are looking for
 for m in range(len(sampleDict)):
 	if keyvalue in sampleDict[m]['product']['attributes']:
 		if sampleDict[m]['product']['attributes']['operatingSystem'] == os:
 			if sampleDict[m]['product']['attributes']['location'] == region:
 				if term in sampleDict[m]['terms']:
-					ext = (sampleDict[m]['terms']['OnDemand']['22VPGB87R2NZ8A4A.JRTCKXETXF']['priceDimensions']['22VPGB87R2NZ8A4A.JRTCKXETXF.6YS6EN2CT7']['pricePerUnit']['USD'])
+					unitprice = (sampleDict[m]['terms']['OnDemand']['22VPGB87R2NZ8A4A.JRTCKXETXF']['priceDimensions']['22VPGB87R2NZ8A4A.JRTCKXETXF.6YS6EN2CT7']['pricePerUnit']['USD'])
 				else:
-					print("not found")
+					print("keyvalue not found or does not meet requirements")
 
 
-#maths - hours x days x count)
-sums = round((float(ext)) * 24 * 28 * fmc, 2)
+#Calculation - hours x days x count)
+calc = round((float(unitprice)) * 24 * 28 * fmctrl, 2)
 
 
-print("Cost per month for", "frobnicate_controller_instance_type:", sampleDict[m]['product']['attributes']['instanceType'], '$',sums)
-# print(sampleDict[m]['product']['attributes']['instanceType'])
-# print(sums)
+print("Cost per month for", "frobnicate_controller_instance_type:", sampleDict[m]['product']['attributes']['instanceType'], '$',calc)
