b44ad386f04477f795ae5441b09fdce46e5e5223
Guy Tallon <guy.tallon@benevolent.ai>
2021-02-19 15:37:00 +0000

    chaged to eu-west and fixed to take instance

--- aws_calc_function.py
+++ aws_calc_function.py
@@ -2,27 +2,23 @@ import json
 
 print("Prices of AWS")
 
+os = 'SUSE'
+os2 = 'RHEL'
 
-with open('snippet.json') as data:
+with open('eu-west-2.json') as data:
   sampleDict = json.load(data)
 
 #search through nested json for instancetype and return if instance type is present
 def search_aws (instance):
 	for m in range(len(sampleDict)):
 		if instance in sampleDict[m]['product']['attributes']:
-			return sampleDict[m]['product']['attributes']['instanceType']
-		else:
-			print("Keyvalue not present")
+			if sampleDict[m]['product']['attributes']['operatingSystem'] == os:
+				return sampleDict[m]['product']['attributes']['operatingSystem']
+			else:
+				print("The OS is", sampleDict[m]['product']['attributes']['operatingSystem'])
 
-
-aws_instance = input("The type of instance. e.g. m3.large: ")
-# vmtype = 't2.xlarge'
 
 #print the instance type for a check.
 print("Instance Type:")
 print(search_aws("instanceType"))
-
-# print(sampleDict[m]['product']['attributes']['instanceType'])
-
-#for the instnace type return the  0 product - terms - OnDemand - PriceperUnit
 
