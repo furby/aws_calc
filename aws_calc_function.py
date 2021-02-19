ac5cf126c7fcd0e1f3a2a55439aca78d58bad1f6
Guy Tallon <guy.tallon@benevolent.ai>
2021-02-19 11:38:37 +0000

    renamed and comment updated

--- aws_calc_function.py
+++ aws_calc_function.py
@@ -1,50 +1,26 @@
-#Import JSON module
 import json
-
-#varibles
-#frobnicate_controller_instance_type
-fc='r5.2xlarge'
-#frobnicate_multi_controller
-fmc=2
-#frobnicate_worker_count
-fwc=5
-#frobnicate_worker_instance_type
-fw='m5.xlarge'
-#smash_controller_instance_type
-sc='m5.xlarge'
-#smash_worker_count
-swc=30
-#smash_worker_instance_type
-sworker='i3.2xlarge'
-
-os = "Windows"
-term = 'OnDemand'
-region = "EU (London)"
-
-instance = input("The type of instance. e.g. t2.xlarge: ")
-keyvalue = 'instanceType'
-
 
 print("Prices of AWS")
 
-#open JSON file and assign variable
+
 with open('snippet.json') as data:
   sampleDict = json.load(data)
 
-for m in range(len(sampleDict)):
-	if keyvalue in sampleDict[m]['product']['attributes']:
-		if sampleDict[m]['product']['attributes']['operatingSystem'] == os:
-			if sampleDict[m]['product']['attributes']['location'] == region:
-				if term in sampleDict[m]['terms']:
-					ext = (sampleDict[m]['terms']['OnDemand']['22VPGB87R2NZ8A4A.JRTCKXETXF']['priceDimensions']['22VPGB87R2NZ8A4A.JRTCKXETXF.6YS6EN2CT7']['pricePerUnit']['USD'])
-				else:
-					print("not found")
+#search through nested json for instancetype and return if instance type is present
+def search_aws (vmtype):
+ for m in range(len(sampleDict)):
+  if 'instanceType' in sampleDict[m]['product']['attributes']:
+    return sampleDict[m]['product']['attributes']['instanceType']
 
 
-#maths - hours x days
-sums = round((float(ext)) * 24 * 28 * fmc, 2)
+vmtype = input("The type of instance. e.g. m3.large: ")
+# vmtype = 't2.xlarge'
 
+#print the instance type for a check.
+print("Instance Type:")
+print(search_aws(vmtype))
 
-print("Cost per month for", "frobnicate_controller_instance_type:", sampleDict[m]['product']['attributes']['instanceType'], '$',sums)
 # print(sampleDict[m]['product']['attributes']['instanceType'])
-# print(sums)
+
+#for the instnace type return the  0 product - terms - OnDemand - PriceperUnit
+
