1e3c9f8e6108b0b1da9383f86b0678131bf4d957
Guy Tallon <guy.tallon@benevolent.ai>
2021-02-19 14:51:36 +0000

    var name changes and comments removed

--- aws_calc_function.py
+++ aws_calc_function.py
@@ -7,18 +7,20 @@ with open('snippet.json') as data:
   sampleDict = json.load(data)
 
 #search through nested json for instancetype and return if instance type is present
-def search_aws (vmtype):
- for m in range(len(sampleDict)):
-  if 'instanceType' in sampleDict[m]['product']['attributes']:
-    return sampleDict[m]['product']['attributes']['instanceType']
+def search_aws (instance):
+	for m in range(len(sampleDict)):
+		if instance in sampleDict[m]['product']['attributes']:
+			return sampleDict[m]['product']['attributes']['instanceType']
+		else:
+			print("Keyvalue not present")
 
 
-vmtype = input("The type of instance. e.g. m3.large: ")
+aws_instance = input("The type of instance. e.g. m3.large: ")
 # vmtype = 't2.xlarge'
 
 #print the instance type for a check.
 print("Instance Type:")
-print(search_aws(vmtype))
+print(search_aws("instanceType"))
 
 # print(sampleDict[m]['product']['attributes']['instanceType'])
 
